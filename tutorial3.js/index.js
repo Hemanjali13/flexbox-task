c83c76d233a019dd8266b9339cd54d88383dc951
Hemanjali <hemanjali1777@gmail.com>
2023-03-09 12:12:53 +0530

    basic exmples for all js topics

---
+++ tutorial3.js/index.js
@@ -0,0 +1,11 @@
+
+
+var x=9
+
+function getName() {
+    console.log("Hello World");
+}
+
+getName();
+console.log(x);
+console.log(getName);
---
+++ tutorial5.js/index.js
@@ -0,0 +1,6 @@
+var a=9;
+console.log(a);
+a=15;
+console.log(a);
+a="hello world";
+console.log(a);
